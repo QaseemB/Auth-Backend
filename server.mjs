3eb375d18973c5ab13c0a62faa2a19cbeb0fee2b
QaseemB <Qbarnhardt@yahoo.com>
2025-06-04 18:01:34 -0400

    setting up server

---
+++ server.mjs
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import {config} from './config/config.mjs 
+
+
+const app = express();
+
+
+
+
+const PORT = config.PORT || 3030;
+app.listen (PORT, () => {
+  console.log('Server is running on port ${PORT}');
+});
+
+
