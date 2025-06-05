ab3f4340ed1c6f3d53d0eaa2b0f8731880b52d15
QaseemB <Qbarnhardt@yahoo.com>
2025-06-05 18:56:48 -0400

    passport

---
+++ DB.mjs
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import config from "./config/config.mjs"
+
+ mongoose.connection.on("connected, () => {
+ console.log(Mongoose connected to database.");
+  });
+  mongoose.connection.on("error",(err) => {
+    console.error("Mongoose conecction error:", err);
+  });
+  mongoose.connection.on("disconnected,() => {
+    console.log("Mongoose disconnected.");
+  });
+
+const DB = async () => {
+  try {
+     await mongoose.connect(config.MONGO_URI,{
+        serverSelectionTimeoutMS; 3000,
+        socketTimeoutMS: 45000,
+     });
+    console.log("Database conection established. staring server...")
+  } catch (err) {
+    console.error("Error connection to MOngoDB:", err);
+    process.exit(1);
+   }
+};
+
+export default DB;
--- server.mjs
+++ server.mjs
@@ -4,24 +4,61 @@ import dotenv from 'dotenv';
 import {config} from './config/config.mjs'; 
 import session from 'express-session';
 import passport from 'passport';
+import GoogleStrategy from 'passport-google-oauth20'
 import MongoStore from 'connect-mongo';
-
-
+import LocalStrategy from 'passport-local'
+import DB
 const app = express();
-
-
-app.get('/', (req, res) =>{
-  res.send('Hello World')
-});
 
 mongoose.connect(config.MONGO_URI,{
   useNewUrlParser:true,
   useUnifiedTopology: true
 }
 
+app.use(session({
+  secret: "secret",
+  resave: false,
+  saveUnintialized: true,
+}))
+
+
+
+app.use(passport.initalize())
+app.use(passport.session())
+app.use(new LocalStrategy (authUser))
+
+
+passport.use( new GoogleStrategy({
+  clientID: config.GOOGLE_CLIENT_ID,
+  clientSecret:config.GOOGLE_CLIENT_SECRET,
+  callbackURL: "/auth/google/callback".
+  passReqToCallback: true
+ },
+  function(request, accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ googleID: profile.id}, function (err,user){
+      return done(err,user);
+    });
+   }
+));
+
+app.get('/', (req, res) =>{
+  res.send('Hello World')
+});
+
+app.get('/auth/google',
+  passport.authenticate('google', {scope:
+    ['email','profile'] }
+  ));
+
+app.get ('auth/google/callback',
+  passport.authenticate( 'google', {
+    successRedirect: '/auth/google/success',
+    failureRedirect: '/auth/google/failure
+ }));
+
 const PORT = config.PORT || 3030;
 app.listen (PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+e console.log(`Server is running on port ${PORT}`);
 });
 
 
